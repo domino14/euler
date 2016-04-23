92c1ba1b044619b174cb3a0f35a58a622ed073e3
Cesar Del Solar <delsolar@gmail.com>
2016-04-22 23:56:06 -0700

    meh

--- 31.py
+++ 31.py
@@ -4,7 +4,12 @@
 # from least to greatest:
 # 3_1_1_0_2_1_1_0
 import math
-ways = set()
+ways = 0
+
+
+def add_to_ways(c1, c2, c5, c10, c20, c50, c100, c200):
+    ways.add('{0}_{1}_{2}_{3}_{4}_{5}_{6}_{7}'.format(
+        c1, c2, c5, c10, c20, c50, c100, c200))
 
 for c1 in range(201):
     print c1
@@ -15,8 +20,9 @@ for c1 in range(201):
                     for c50 in range(5 - int(math.ceil(c20*20/50.))):
                         for c100 in range(3 - int(math.ceil(c50*50/100.))):
                             for c200 in range(2 - int(math.ceil(c100*100/200.))):
-                                if c200*200 + c100*100 + c50*50 + c20*20 + c10*10 + c5*5 + c2*2 + c1*1 == 200:
-                                    ways.add('{0}_{1}_{2}_{3}_{4}_{5}_{6}_{7}'.format(
-                                            c1, c2, c5, c10, c20, c50, c100, c200))
-
-print len(ways)
+                                if (c200*200 + c100*100 + c50*50 + c20*20 +
+                                        c10*10 + c5*5 + c2*2 + c1*1 == 200):
+                                    # add_to_ways(c1, c2, c5, c10, c20, c50,
+                                    #             c100, c200)
+                                    ways += 1
+print ways
